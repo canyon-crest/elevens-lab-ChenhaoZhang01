efaa64f0af8d55d4e5e85b218ddb31297fa17d46
Chenhao Zhang <chenhaozhang01@gmail.com>
2025-10-30 09:37:17 -0700

    Implement DeckTester2 main method functionality
    Initialized Deck2 with ranks, suits, and point values. Added print statements to check deck operations.

--- src/main/java/Activity2/DeckTester2.java
+++ src/main/java/Activity2/DeckTester2.java
@@ -9,6 +9,13 @@ public class DeckTester2 {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		String[] ranks = {"jack", "queen", "king"};
+		String[] suits = {"blue", "red"};
+		int[] pointValues = {11, 12, 13};
+		Deck2 d = new Deck2(ranks, suits, pointValues);
+		System.out.println(d.toString());
+		System.out.println(d.isEmpty());
+		System.out.println(d.size());
+		System.out.println(d.deal());
 	}
 }
